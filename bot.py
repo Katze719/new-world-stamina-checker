1d36daf9344783b2cf15e0a7f195d01d4d90d72e
Katze719 <pauldorn1234@gmail.com>
2025-02-09 00:57:07 +0000

    add changelog

--- bot.py
+++ bot.py
@@ -366,5 +366,27 @@ async def on_message(message: discord.Message):
 
         await message.channel.send(embed=embed)
 
+def parse_changelog():
+    try:
+        with open('CHANGELOG.txt', 'r') as file:
+            content = file.read()
+            end_index = content.find('---')
+            if end_index != -1:
+                latest_entry = content[:end_index].strip()
+            else:
+                latest_entry = content.strip()
+            return latest_entry
+    except FileNotFoundError:
+        return "CHANGELOG.txt file not found."
+    except Exception as e:
+        return f"Error reading the changelog: {str(e)}"
+    
+@tree.command(name="changelog", description="Gives you the latest changelog entry.")
+async def changelog(ctx: discord.Interaction):
+    latest_changelog = parse_changelog()
+    embed = discord.Embed(title="Latest Changelog", description=f"[View the full changelog here](https://github.com/Katze719/new-world-stamina-checker/blob/main/CHANGELOG.txt)\n\n{latest_changelog}", color=discord.Color.blue())
+    if len(latest_changelog) > 2048:
+        latest_changelog = latest_changelog[:2045] + "..."
+    await ctx.response.send_message(embed=embed)
 
 bot.run(DISCORD_TOKEN)
